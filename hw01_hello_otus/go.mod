58f919973178a3621f73b3f2209339da3a519141
a.tsiporin <a.tsiporin@pravo.tech>
2024-02-01 22:28:16 +0500

    Применил golang.org/x/example/hello/reverse

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,5 @@
 module github.com/trb1maker/otus_golang_home_work/hw01_hello_otus
 
 go 1.19
+
+require golang.org/x/example/hello v0.0.0-20231013143937-1d6d2400d402
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"golang.org/x/example/hello/reverse"
 )
 
 func main() {
-	// const hello = "Hello, OTUS!"
-	fmt.Print("!SUTO ,olleH")
+	const hello = "Hello, OTUS!"
+	fmt.Print(reverse.String(hello))
 }
