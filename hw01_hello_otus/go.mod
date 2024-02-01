29662b674e4f4827efbc905e4d54744359938630
a.tsiporin <a.tsiporin@pravo.tech>
2024-02-01 22:17:09 +0500

    Базовая модель. Жду ответа на вопрос о необходимости применения функции reverse.

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/trb1maker/otus_golang_home_work/hw01_hello_otus
 
 go 1.19
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	// Place your code here.
+	// const hello = "Hello, OTUS!"
+	fmt.Print("!SUTO ,olleH")
 }
