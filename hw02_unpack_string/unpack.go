4ec406ff862cb096a1796c638474f6f7be65d6b7
a.tsiporin <a.tsiporin@pravo.tech>
2024-02-10 21:30:50 +0500

    Реализивал Unpack в базовом варианте

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var (
+		result    = new(strings.Builder)
+		items     = []rune(s)
+		lastIndex = len(items) - 1
+	)
+	for i := 0; i < len(items); {
+		// Текущий символ не может быть числом
+		item := items[i]
+		if unicode.IsDigit(item) {
+			return "", ErrInvalidString
+		}
+		// Последний символ просто записываю в результат
+		if i == lastIndex {
+			result.WriteRune(item)
+			break
+		}
+		// Если ошибок нет, то проверяю, является ли следующий символ числом
+		next := items[i+1]
+		if unicode.IsDigit(next) {
+			count, err := strconv.Atoi(string(next))
+			if err != nil {
+				panic(err)
+			}
+			result.WriteString(strings.Repeat(string(item), count))
+			// Если следующий символ число, то выполнив распаковку я пропускаю уже обработанный символ
+			i += 2
+			continue
+		}
+		// Если следующий символ буква, то сохраняю текущий символ и перехожу к обработке следующего
+		result.WriteRune(item)
+		i++
+	}
+	return result.String(), nil
 }
