b7e4532f920e636dced654925fe7fe47c5ceb064
a.tsiporin <a.tsiporin@pravo.tech>
2024-02-11 00:07:07 +0500

    Реализовано решение задачи в базовом варианте

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,15 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"strings"
+
+	"github.com/trb1maker/otus_golang_home_work/hw03_frequency_analysis/counter"
+)
+
+func Top10(s string) []string {
+	c := make(counter.Counter)
+	for _, w := range strings.Fields(s) {
+		c[w]++
+	}
+	return c.GetTop()
 }
