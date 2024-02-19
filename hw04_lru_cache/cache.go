7106d72be90784cfb17fefff57fbd8e773db6ce8
a.tsiporin <a.tsiporin@pravo.tech>
2024-02-19 10:49:29 +0500

    Реализовал базовый функционал кэша

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,35 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	if i, ok := l.items[key]; ok {
+		l.queue.MoveToFront(i)
+		i.Value = value
+		return true
+	}
+	if l.queue.Len() == l.capacity {
+		l.queue.Remove(l.queue.Back())
+	}
+	l.items[key] = l.queue.PushFront(value)
+	return false
+}
+
+func (l *lruCache) Get(key Key) (interface{}, bool) {
+	if i, ok := l.items[key]; ok {
+		l.queue.MoveToFront(i)
+		return i.Value, true
+	}
+	return nil, false
+}
+
+func (l *lruCache) Clear() {
+	l.items = make(map[Key]*ListItem, l.capacity)
+	l.queue = NewList()
 }
 
 func NewCache(capacity int) Cache {
