537de1e89f63ec6f0bd7a5892833337b92366f49
a.tsiporin <a.tsiporin@pravo.tech>
2024-03-02 18:48:18 +0500

    Основная задача решена

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,9 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"sync"
 )
 
 var (
@@ -18,5 +20,66 @@ func Run(tasks []Task, workersCount, maxErrorsCount int) error {
 	if maxErrorsCount <= 0 {
 		return ErrErrorsLimitExceeded
 	}
-	return nil
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var err error
+	for range waitErrors(ctx, tasks, workersCount) {
+		maxErrorsCount--
+		if maxErrorsCount == 0 {
+			cancel()
+			err = ErrErrorsLimitExceeded
+		}
+	}
+	return err
+}
+
+// waitErrors запускает taskChannel и набор worker, возвращает канал ошибок
+func waitErrors(ctx context.Context, tt []Task, workersCount int) <-chan error {
+	out := make(chan error)
+	wg := &sync.WaitGroup{}
+	wg.Add(workersCount + 1)
+	go func() {
+		defer close(out)
+		wg.Wait()
+	}()
+	go func() {
+		tasks := taskChannel(ctx, tt, wg)
+		for i := 0; i < workersCount; i++ {
+			go worker(ctx, tasks, out, wg)
+		}
+	}()
+	return out
+}
+
+// taskChannel отправляет задачи в канал пока не получит сигнал завершить работу или пока вся работа не будет завершена
+func taskChannel(ctx context.Context, tt []Task, wg *sync.WaitGroup) <-chan Task {
+	out := make(chan Task)
+	go func() {
+		defer wg.Done()
+		defer close(out)
+		for _, t := range tt {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				out <- t
+			}
+		}
+	}()
+	return out
+}
+
+// worker получает задачи из канала и выполняет их, если не поступил сигнал завершить работу
+func worker(ctx context.Context, tt <-chan Task, errs chan<- error, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for t := range tt {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if err := t(); err != nil {
+				errs <- err
+			}
+		}
+	}
 }
