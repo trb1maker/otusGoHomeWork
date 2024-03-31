6606818a54628ff21d795edf4d7647bb1840717c
a.tsiporin <a.tsiporin@pravo.tech>
2024-03-31 18:55:52 +0500

    Кастомный progress bar

---
+++ hw07_file_copying/progressbar/progressbar_test.go
@@ -0,0 +1,33 @@
+package progressbar
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPrintProgress(t *testing.T) {
+	tests := []struct {
+		percent int64
+		want    string
+	}{
+		{0, "\r[                                                                                                    ] 0%"},
+		{10, "\r[||||||||||                                                                                          ] 10%"},
+		{100, "\r[||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||] 100%"},
+	}
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("percent=%d", tt.percent), func(t *testing.T) {
+			got := printProgress(tt.percent)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+func TestProgressBar(t *testing.T) {
+	buf := bytes.NewBuffer(nil)
+
+	pb := &ProgressBar{w: buf, limit: 50}
+	pb.Write([]byte{1, 2, 3, 4, 5})
+	require.Equal(t, "\r[||||||||||                                                                                          ] 10%", buf.String())
+}
