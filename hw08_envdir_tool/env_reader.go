47a73655d9e96eb70b713199f187581c9a10e22b
a.tsiporin <a.tsiporin@pravo.tech>
2024-04-07 22:01:52 +0500

    ReadDir with tests is compleated

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +17,56 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	env := make(Environment)
+
+	for _, file := range files {
+
+		// имя переменной не должно содержать символа "="
+		if strings.Contains(file.Name(), "=") {
+			continue
+		}
+
+		// если файл полностью пустой, помечаю переменную на удаление
+		stat, err := file.Info()
+		if err != nil {
+			return nil, err
+		}
+		if stat.Size() == 0 {
+			env[file.Name()] = EnvValue{NeedRemove: true}
+			continue
+		}
+
+		value, err := readEnvFromFile(dir + "/" + file.Name())
+		if err != nil {
+			return nil, err
+		}
+		env[file.Name()] = value
+	}
+
+	return env, nil
+}
+
+func readEnvFromFile(name string) (EnvValue, error) {
+	data, err := os.ReadFile(name)
+	if err != nil {
+		return EnvValue{}, err
+	}
+	return readEnvFromBytes(data)
+}
+
+func readEnvFromBytes(data []byte) (EnvValue, error) {
+	// первая строка в файле - это значение переменной
+	data = bytes.Split(data, []byte{'\n'})[0]
+
+	// пробелы и символы табуляции в конце значения переменной должны быть удалены
+	data = bytes.TrimRight(data, " \t\n")
+
+	// терминальные нули должны быть трансформированы в символ перевода строки
+	data = bytes.ReplaceAll(data, []byte{0}, []byte{'\n'})
+
+	return EnvValue{Value: string(data)}, nil
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,82 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadEnvFromBytes(t *testing.T) {
+	t.Run("первая строка в файле - это значение переменной", func(t *testing.T) {
+		tests := []struct {
+			name  string
+			input []byte
+			want  string
+		}{
+			{"одна строка", []byte("test"), "test"},
+			{"несколько строк", []byte("test1\ntest2"), "test1"},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := readEnvFromBytes(tt.input)
+				require.NoError(t, err)
+				require.Equal(t, tt.want, got.Value)
+			})
+		}
+	})
+
+	t.Run("пробелы и символы табуляции в конце значения переменной должны быть удалены", func(t *testing.T) {
+		tests := []struct {
+			name  string
+			input []byte
+			want  string
+		}{
+			{"пробелы", []byte("test "), "test"},
+			{"табуляции", []byte("test\t"), "test"},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := readEnvFromBytes(tt.input)
+				require.NoError(t, err)
+				require.Equal(t, tt.want, got.Value)
+			})
+		}
+	})
+
+	t.Run("терминальные нули должны быть трансформированы в символ перевода строки", func(t *testing.T) {
+		tests := []struct {
+			name  string
+			input []byte
+			want  string
+		}{
+			{"терминальные нули", []byte("test\000"), "test\n"},
+		}
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := readEnvFromBytes(tt.input)
+				require.NoError(t, err)
+				require.Equal(t, tt.want, got.Value)
+			})
+		}
+	})
+
+	t.Run("комплексный тест", func(t *testing.T) {
+		test := []byte("a\000b \t\nc")
+		got, err := readEnvFromBytes(test)
+		require.NoError(t, err)
+		require.Equal(t, "a\nb", got.Value)
+	})
+}
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	env, err := ReadDir("testdata/env")
+	require.NoError(t, err)
+	test := Environment{
+		"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+		"EMPTY": EnvValue{Value: "", NeedRemove: false},
+		"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+		"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+		"UNSET": EnvValue{Value: "", NeedRemove: true},
+	}
+	require.Equal(t, test, env)
 }
