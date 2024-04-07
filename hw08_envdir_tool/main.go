f54f05e83e116357cd2fa1106c98272377cd16a1
a.tsiporin <a.tsiporin@pravo.tech>
2024-04-07 23:17:27 +0500

    main is comleated

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	envDir := os.Args[1]
+	args := os.Args[2:]
+
+	env, err := ReadDir(envDir)
+	if err != nil {
+		slog.Error("failed to read env", err)
+		return
+	}
+
+	for name, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(name)
+		}
+		// По идее здесь бы добавить переменные в окружение, то тогда нет смысла
+		// передавать их в командную RunCmd в качестве аргумента.
+	}
+
+	returnCode := RunCmd(args, env)
+	os.Exit(returnCode)
 }
