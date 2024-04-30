7e04d0c3b1655f6522f9e8d528a1bd1055578ae8
a.tsiporin <a.tsiporin@pravo.tech>
2024-04-30 17:21:00 +0500

    Реализваны правила валидирования.

---
+++ hw09_struct_validator/rules/length_test.go
@@ -0,0 +1,26 @@
+package rules
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLength(t *testing.T) {
+	t.Run("valid length", func(t *testing.T) {
+		r, err := newLengthValidator("4")
+		require.NoError(t, err)
+		require.NoError(t, r.Validate("abcd"))
+	})
+	t.Run("invalid length", func(t *testing.T) {
+		r, err := newLengthValidator("10")
+		require.NoError(t, err)
+		require.Error(t, r.Validate("abcd"))
+	})
+	t.Run("invalid tag", func(t *testing.T) {
+		r, err := newLengthValidator("a")
+		require.Error(t, err)
+		require.ErrorIs(t, err, ErrNotValidTag)
+		require.Nil(t, r)
+	})
+}
---
+++ hw09_struct_validator/rules/max.go
@@ -0,0 +1,27 @@
+package rules
+
+import (
+	"errors"
+	"strconv"
+)
+
+var ErrMax = errors.New("more then max")
+
+type max struct {
+	max int
+}
+
+func (m *max) Validate(v int) error {
+	if v > m.max {
+		return ErrMax
+	}
+	return nil
+}
+
+func newMaxValidator(s string) (Rule[int], error) {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return nil, ErrNotValidTag
+	}
+	return &max{max: i}, nil
+}
