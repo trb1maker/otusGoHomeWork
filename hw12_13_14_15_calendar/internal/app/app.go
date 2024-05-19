e688135316eca7be55d52533eba80589d71afe20
a.tsiporin <a.tsiporin@pravo.tech>
2024-05-19 12:29:08 +0500

    Реализация базовых методов App.

--- hw12_13_14_15_calendar/internal/app/app.go
+++ hw12_13_14_15_calendar/internal/app/app.go
@@ -2,25 +2,62 @@ package app
 
 import (
 	"context"
+	"time"
+
+	"github.com/trb1maker/otus_golang_home_work/hw12_13_14_15_calendar/internal/storage"
 )
 
-type App struct { // TODO
+type App struct {
+	store Storage
 }
 
-type Logger interface { // TODO
+type Storage interface {
+	InsertOne(context.Context, storage.Event) (string, error)
+	SelectOne(context.Context, string) (storage.Event, error)
+	UpdateOne(context.Context, storage.Event) error
+	DeleteOne(context.Context, string) error
 }
 
-type Storage interface { // TODO
+func New(storage Storage) *App {
+	return &App{
+		store: storage,
+	}
 }
 
-func New(logger Logger, storage Storage) *App {
-	return &App{}
+// TODO: типы аргументов должны соответствовать возможностям HTTP и GRPC,
+// на текущий момент я реализую их в соответствии с типами Event,
+// позже, возможно поменяю.
+
+func (a *App) CreateEvent(
+	ctx context.Context,
+	userID string,
+	title string,
+	startTime time.Time,
+	endTime time.Time,
+	description string,
+	notify time.Duration,
+) (string, error) {
+	e := storage.Event{
+		Title:       title,
+		StartTime:   startTime,
+		EndTime:     endTime,
+		Description: description,
+		OwnerID:     userID,
+		Notify:      notify,
+	}
+	return a.store.InsertOne(ctx, e)
 }
 
-func (a *App) CreateEvent(ctx context.Context, id, title string) error {
-	// TODO
-	return nil
-	// return a.storage.CreateEvent(storage.Event{ID: id, Title: title})
+func (a *App) GetEvent(ctx context.Context, eventID string) (storage.Event, error) {
+	return a.store.SelectOne(ctx, eventID)
 }
 
-// TODO
+func (a *App) UpdateEvent(ctx context.Context, event storage.Event) error {
+	return a.store.UpdateOne(ctx, event)
+}
+
+func (a *App) DeleteEvent(ctx context.Context, eventID string) error {
+	return a.store.DeleteOne(ctx, eventID)
+}
+
+// TODO: Реализовать другие методы приложения после GRPC
