bdba2ec7bf8aba4907cde29dc4bb7f042c3de21c
a.tsiporin <a.tsiporin@pravo.tech>
2024-05-23 10:00:47 +0500

    Рефакторинг конфигурации.
    Убрал передачу параметров конфигурации в переменные окружения, оставил
    инициализацию логгера.

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -18,11 +18,9 @@ func loadConfig(fileName string) (*config, error) {
 		return nil, err
 	}
 
-	if err := conf.init(); err != nil {
-		return nil, err
-	}
+	conf.Logger.init()
 
-	return &conf, err
+	return &conf, nil
 }
 
 type config struct {
@@ -31,31 +29,8 @@ type config struct {
 	Logger  loggerConf  `yaml:"logger"`
 }
 
-func (c *config) init() error {
-	if err := c.Server.init(); err != nil {
-		return err
-	}
-
-	if err := c.Storage.init(); err != nil {
-		return err
-	}
-
-	c.Logger.init()
-	return nil
-}
-
 type serverConf struct {
 	HTTP *httpConf `yaml:"http"`
-}
-
-func (s *serverConf) init() error {
-	if s.HTTP != nil {
-		if err := s.HTTP.init(); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 type httpConf struct {
@@ -63,60 +38,17 @@ type httpConf struct {
 	Port int    `yaml:"port"`
 }
 
-func (h *httpConf) init() error {
-	if err := os.Setenv("SERVERHOST", h.Host); err != nil {
-		return err
-	}
-
-	if err := os.Setenv("SERVERPORT", strconv.Itoa(h.Port)); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 type storageConf struct {
 	Type     string        `yaml:"type"`
 	Postgres *postgresConf `yaml:"postgres"`
 }
 
-func (s *storageConf) init() error {
-	if s.Type == "postgres" {
-		return s.Postgres.init()
-	}
-	return nil
-}
-
 type postgresConf struct {
 	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
+	Port     uint16 `yaml:"port"`
 	Database string `yaml:"database"`
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
-}
-
-func (p *postgresConf) init() error {
-	if err := os.Setenv("DBHOST", p.Host); err != nil {
-		return err
-	}
-
-	if err := os.Setenv("DBPORT", strconv.Itoa(p.Port)); err != nil {
-		return err
-	}
-
-	if err := os.Setenv("DBNAME", p.Database); err != nil {
-		return err
-	}
-
-	if err := os.Setenv("DBUSER", p.User); err != nil {
-		return err
-	}
-
-	if err := os.Setenv("DBPASSWORD", p.Password); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 type loggerConf struct {
